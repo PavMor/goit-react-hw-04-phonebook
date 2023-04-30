40f804d7d889ba95f02f9140a0473a433b2f4ded
Your Name <pavlo.morozenko@ukr.net>
2023-04-30 12:33:49 +0200

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import { ContactForm } from './ContactForm/ContactForm';
@@ -7,14 +7,22 @@ import { Filter } from './Filter/Filter';
 import { Message } from './Message/Message';
 import { Container, Title, TitleСolor, Subtitle } from './App.styled';
 
-export const App = class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  addContact = ({ name, number }) => {
-    const { contacts } = this.state;
+  useEffect(() => {
+    const contacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(contacts);
+
+    parsedContacts ? setContacts(parsedContacts) : setContacts([]);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
     const newContact = { id: nanoid(), name, number };
 
     contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())
@@ -23,70 +31,42 @@ export const App = class App extends Component {
           'This user is already in the contact list.',
           'OK'
         )
-      : this.setState(({ contacts }) => ({
-          contacts: [newContact, ...contacts],
-        }));
+      : setContacts(prevContacts => [newContact, ...prevContacts]);
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(prevContacts => 
+      prevContacts.filter(contact => contact.id !== contactId),
+    );
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  filteredContacts = () => {
-    const { filter, contacts } = this.state;
+  const filteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedFilter)
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
+  return (
+    <Container>
+      <Title>
+        Phone<TitleСolor>book</TitleСolor>
+      </Title>
+      <ContactForm onSubmit={addContact} />
 
-  componentDidMount() {
-    const contacts = localStorage.getItem("contacts");
-    const parsedContacts = JSON.parse(contacts);
-
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-    }
-  }
-
-  render() {
-    const { filter } = this.state;
-    const addContact = this.addContact;
-    const changeFilter = this.changeFilter;
-    const filteredContacts = this.filteredContacts();
-    const deleteContact = this.deleteContact;
-    const length = this.state.contacts.length;
-
-    return (
-      <Container>
-        <Title>
-          Phone<TitleСolor>book</TitleСolor>
-        </Title>
-        <ContactForm onSubmit={addContact} />
-
-        <Subtitle>Contacts</Subtitle>
-        <Filter filter={filter} changeFilter={changeFilter} />
-        {length > 0 ? (
-          <ContactList
-            contacts={filteredContacts}
-            onDeleteContact={deleteContact}
-          />
-        ) : (
-          <Message text="Contact list is empty." />
-        )}
-      </Container>
-    );
-  }
+      <Subtitle>Contacts</Subtitle>
+      <Filter filter={filter} changeFilter={changeFilter} />
+      {contacts.length > 0 ? (
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={deleteContact}
+        />
+      ) : (
+        <Message text="Contact list is empty." />
+      )}
+    </Container>
+  );
 }
